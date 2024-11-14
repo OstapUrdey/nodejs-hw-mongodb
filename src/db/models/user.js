988b85a4a0b9e6c2b364c9de1b83bdd053608efe
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-11-14 16:07:41 +0200

    step 1 is complate

---
+++ src/db/models/user.js
@@ -0,0 +1,21 @@
+import {model, Schema} from 'mongoose';
+
+const usersSchema = new Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    }, {timestamps: true, versionKey: false},
+);
+
+export const UserCollection = model("users", usersSchema);
