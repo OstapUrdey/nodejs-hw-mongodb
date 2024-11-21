1a2a6a5807d3cf8db430434c42f5d51711c2edc4
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-11-21 21:47:57 +0200

    update user.js

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,5 +1,5 @@
 import {model, Schema} from 'mongoose';
-import { emailRegexp, ROLES } from '../../constants/index.js';
+import { emailRegexp } from '../../constants/index.js';
 
 const usersSchema = new Schema({
         name: {
@@ -15,11 +15,6 @@ const usersSchema = new Schema({
         password: {
             type: String,
             required: true
-        },
-        role: {
-            type: String,
-            enum: [ROLES.ADMIN, ROLES.GUEST],
-            default: ROLES.GUEST,
         },
     }, {timestamps: true, versionKey: false},
 );
