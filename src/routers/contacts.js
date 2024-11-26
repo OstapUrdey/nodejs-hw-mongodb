d68e539165fd97c2d8013f3dd35fa20afd5c90b1
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-11-26 19:45:20 +0200

    update

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,7 +4,7 @@ import {ctrlWrapper} from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
-import { authenticate } from '../middlewares/aunthenticate.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
